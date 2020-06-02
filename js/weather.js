2f866e9c78d80b6fb68dd4133f2c60dab487793a
nickmackenzie <54223351+nickmackenzie@users.noreply.github.com>
2020-06-02 16:59:36 -0400

    master

---
+++ js/weather.js
@@ -0,0 +1,42 @@
+let weatherIcon = document.getElementById("weather-icon");
+let temperature = document.getElementById("temperature");
+let city = document.getElementById("city");
+let country = document.getElementById("country");
+let notification = document.getElementById("notification");
+
+const key = "b5decc838911549f995fee61f543d4f0";
+
+//check browser support
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${key}`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const { temp } = data.main;
+          const { country } = data.sys;
+          const { name } = data;
+          const { icon } = data.weather[0];
+          const iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
+          temperature.textContent = temp + "°";
+          city.textContent = name + ",";
+          country.textContent = country;
+          weatherIcon.innerHTML = "<img src={iconUrl}>";
+          console.log(temp);
+          console.log(name);
+          console.log(country);
+          console.log(iconUrl);
+        });
+    });
+  }
+});
