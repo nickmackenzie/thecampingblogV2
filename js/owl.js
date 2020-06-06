c3dbbfc723ef2dd3f585c6810b9a1da2a421944b
nickmackenzie <54223351+nickmackenzie@users.noreply.github.com>
2020-06-06 12:46:54 -0400

    mobile stuff

--- js/owl.js
+++
@@ -1,1 +0,0 @@
-let paddle = getElement;
--- js/paddle.js
+++ js/paddle.js
@@ -1,1 +1,23 @@
-let paddleOne = 
+
+/* Selectors */
+let paddle1 = document.querySelector(".paddle-icon")
+let paddle2 = document.querySelector('.paddle-icon-2')
+let paddleContainer = document.querySelector('.paddle')
+let list = document.querySelector(".list")
+
+
+/* Event Listeners */
+paddleContainer.addEventListener('click', paddleBurger)
+
+/* Functions */
+function paddleBurger() {
+    if (paddleContainer.className === 'paddle'){
+        paddleContainer.className = 'active';
+        list.classList.add("nav-active")
+      } else {
+        paddleContainer.className = 'paddle';
+        list.classList.remove("nav-active")
+      }
+}
+
+
